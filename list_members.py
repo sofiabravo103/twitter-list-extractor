d2bb799b4c8c8010ad932a15224fb5a37e4e1c14
Sofia Bravo <sofiabravo103@gmail.com>
2015-04-29 14:14:13 -0430

    reading lists from spec file

--- list_members.py
+++ list_members.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 import pickle
 import tweepy
+from time import sleep
 from information_spec import *
 
 MEMBERS_PICKLE_FILE = 'pickles/downloaded_lists_members.p'
@@ -31,13 +33,33 @@ def save_data():
 
 def get_list_users(screen_name, list_name):
   ids = []
-  for user in tweepy.Cursor(api.list_members,screen_name,list_name).items():
-    ids.append(user.id)
+  try:
+    for user in tweepy.Cursor(api.list_members,screen_name,list_name).items():
+      ids.append(user.id)
+    return ids
+  except tweepy.error.TweepError as e:
+    print '\t[DOWNLOAD ERROR] Error with data in list "{0}": {1}'.\
+      format(list_name, e[0][0]['message'])
+    if e[0][0]['code'] == 88:
+      sys.stdout.write('Sleeping for 15 minutes...')
+      sys.stdout.flush()
+      sleep(900)
+      sys.stdout.write(" and I'm back.")
+      sys.stdout.flush()
+      return get_list_users(screen_name, list_name)
 
-  return ids
 
 def main():
-  pass
+  for list_id in spec_lists.keys():
+    if list_id not in downloaded_lists_members:
+      list_data = spec_lists[list_id]
+      print 'Downloading list #{0}, name {1}...'.\
+        format(list_id, list_data['list_name'])
+      users = get_list_users(list_data['screen_name'], list_data['list_name'])
+      if users is not None:
+        downloaded_lists_members[list_id] = users        
+        print 'Done'
+        save_data()
 
 if __name__ == '__main__':
   configure_tweepy()
